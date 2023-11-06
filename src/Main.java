18a4124d6c338447a8e574639fa4dae02da4099e
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-11-06 21:47:11 +0200

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         InferenceEngine inferenceEngine = new InferenceEngine();
         Scanner in = new Scanner(System.in);
+        boolean runing = true;
 
         inferenceEngine.addRule(new ModusPonens());
         inferenceEngine.addRule(new ModusTollens());
@@ -11,21 +12,34 @@ public class Main {
         inferenceEngine.addRule(new DisjunctiveSyllogism());
         inferenceEngine.addRule(new Resolution());
 
-        while (true){
-            System.out.println("Enter first Expression (Empty to stop the program)");
-            String exp1 = in.nextLine();
-            if(exp1.isEmpty()) break;
-            inferenceEngine.addExpression(new Expression(exp1));
+        do {
+            Expression e1, e2;
+            do {
+                System.out.println("Enter first Expression");
+                String exp1 = in.nextLine();
+                e1 = new Expression(exp1);
+                if(!e1.isValid()) System.out.println("invalid Expression");
+            } while (!e1.isValid());
 
-            System.out.println("Enter Second Expression");
-            String exp2 = in.nextLine();
-            inferenceEngine.addExpression(new Expression(exp2));
+            inferenceEngine.addExpression(e1);
+
+            do {
+                System.out.println("Enter Second Expression");
+                String exp2 = in.nextLine();
+                e2 = new Expression(exp2);
+                if(!e2.isValid()) System.out.println("invalid Expression");
+            } while (!e2.isValid());
+
+            inferenceEngine.addExpression(e2);
 
             Expression result = inferenceEngine.applyRules();
-            if(!result.getRepresentation().isEmpty())
+            if (!result.getRepresentation().isEmpty())
                 System.out.println(result.getRepresentation());
             else
                 System.out.println("The input expression cannot be inferred");
-        }
+
+            System.out.println("do you Want to Continue (y/n)");
+
+        } while (in.nextLine().charAt(0) == 'y');
     }
 }
